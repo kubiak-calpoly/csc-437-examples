9a99afd2a9e001d4f1f7abee54995913635ea0a9
Ken Kubiak <kubiak@calpoly.edu>
2023-12-14 08:02:39 -0800

    [mvc] set up nodemon

--- packages/mvc/src/index.ts
+++ packages/mvc/src/index.ts
@@ -3,8 +3,9 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/hello", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+app.get("/hello/:user", (req: Request, res: Response) => {
+  const { user } = req.params;
+  res.send(`Hello there, ${user}!`);
 });
 
 app.listen(port, () => {
