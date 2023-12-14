8915d58256b0d5a423f2d08c0a170c86505adf47
Ken Kubiak <kubiak@calpoly.edu>
2023-12-14 09:53:10 -0800

    serve proto from express as static files

--- packages/mvc/src/index.ts
+++ packages/mvc/src/index.ts
@@ -7,6 +7,18 @@ const eta = new Eta({
 });
 const port = process.env.PORT || 3000;
 
+[
+  "data",
+  "icons",
+  "js",
+  "images",
+  "maps",
+  "pages",
+  "styles"
+].forEach((dir) =>
+  app.use(`/${dir}`, express.static(`static/${dir}`))
+);
+
 app.get("/hello/:name", (req: Request, res: Response) => {
   const { name } = req.params;
   res.send(eta.render("./hello", { name }));
