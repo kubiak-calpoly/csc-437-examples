8ae42483cb4059fcb735e2ba706431312a9353d5
Ken Kubiak <kubiak@calpoly.edu>
2024-02-13 08:57:13 -0800

    use target esnext for vite

--- packages/lit-frontend/vite.config.js
+++ packages/lit-frontend/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    target: "esnext",
     rollupOptions: {
       input: {
         app: resolve(__dirname, "app/index.html")
