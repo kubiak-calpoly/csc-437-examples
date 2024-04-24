e45ec1a0819b6ff5337a275eb6386c0f03c81eb5
Ken Kubiak <kubiak@calpoly.edu>
2024-04-24 11:04:07 -0700

    remove old module 4 files

--- packages/mvc/src/models/Profile.ts
+++
@@ -1,9 +0,0 @@
-export interface Profile {
-  id: string;
-  name: string;
-  nickname: string | undefined;
-  home: string;
-  airports: Array<String>;
-  avatar: string | undefined;
-  color: string | undefined;
-}
