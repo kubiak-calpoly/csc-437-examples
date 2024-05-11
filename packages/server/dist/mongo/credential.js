ad366c67238b94db8330acdf56dfc40712450dbc
Ken Kubiak <kubiak@calpoly.edu>
2024-05-11 11:31:39 -0700

    clanup main branch after mod-6

--- packages/server/dist/mongo/credential.js
+++ packages/server/dist/mongo/credential.js
@@ -3,6 +3,10 @@ var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
 var __copyProps = (to, from, except, desc) => {
   if (from && typeof from === "object" || typeof from === "function") {
     for (let key of __getOwnPropNames(from))
@@ -13,4 +17,27 @@ var __copyProps = (to, from, except, desc) => {
 };
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var credential_exports = {};
+__export(credential_exports, {
+  default: () => credential_default
+});
 module.exports = __toCommonJS(credential_exports);
+var import_mongoose = require("mongoose");
+const credentialSchema = new import_mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    hashedPassword: {
+      type: String,
+      required: true
+    }
+  },
+  { collection: "user_credentials" }
+);
+const credentialModel = (0, import_mongoose.model)(
+  "Credential ",
+  credentialSchema
+);
+var credential_default = credentialModel;
