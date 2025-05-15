c47762875d004e4ef8e74aaa7b26d95571801ab7
Ken Kubiak <kubiak@calpoly.edu>
2025-05-15 11:38:58 -0700

    fix link for venice from itinerary

---
+++ packages/proto/dist/assets/blaze-DUUIz9iO.js
@@ -0,0 +1,1 @@
+import{d as i,c as r}from"./state-By6HLBIX.js";import{H as e}from"./header-BPxdGmxf.js";import{P as a}from"./profile-CNXAyul_.js";i({"blz-header":e,"mu-auth":r.Provider,"profile-view":a});e.initializeOnce();
--- packages/proto/dist/assets/profile-CNXAyul_.js
+++ packages/proto/dist/assets/profile-CNXAyul_.js
@@ -1,4 +1,4 @@
-import{i as $,O as f,d as b,f as y,x as h,r as w,h as k,a as _,b as z,n as v}from"./state-By6HLBIX.js";var A=Object.defineProperty,p=(g,e,t,a)=>{for(var r=void 0,i=g.length-1,s;i>=0;i--)(s=g[i])&&(r=s(e,t,r)||r);return r&&A(e,t,r),r};const u=class u extends ${constructor(){super(...arguments),this.mode="view",this._authObserver=new f(this,"blazing:auth")}render(){return this.mode==="edit"?this.renderEditor():this.renderView()}renderView(){const{userid:e,name:t,nickname:a,home:r,airports:i=[],avatar:s,color:n}=this.traveler||{};return h`
+import{i as $,O as f,d as y,f as b,x as h,r as w,h as k,a as _,b as z,n as v}from"./state-By6HLBIX.js";var C=Object.defineProperty,p=(g,e,t,r)=>{for(var a=void 0,i=g.length-1,s;i>=0;i--)(s=g[i])&&(a=s(e,t,a)||a);return a&&C(e,t,a),a};const u=class u extends ${constructor(){super(...arguments),this.mode="view",this._authObserver=new f(this,"blazing:auth")}render(){return this.mode==="edit"?this.renderEditor():this.renderView()}renderView(){const{userid:e,name:t,nickname:r,home:a,airports:i=[],avatar:s,color:n}=this.traveler||{};return h`
         <button @click=${()=>{this.mode="edit"}}>
           Edit
         </button>
@@ -8,9 +8,9 @@ import{i as $,O as f,d as b,f as y,x as h,r as w,h as k,a as _,b as z,n as v}fro
         <dt>Username</dt>
         <dd>${e}</dd>
         <dt>Nickname</dt>
-        <dd>${a}</dd>
-        <dt>Home City</dt>
         <dd>${r}</dd>
+        <dt>Home City</dt>
+        <dd>${a}</dd>
         <dt>Airports</dt>
         <dd>${i.join(", ")}</dd>
         <dt>Favorite Color</dt>
@@ -24,15 +24,11 @@ import{i as $,O as f,d as b,f as y,x as h,r as w,h as k,a as _,b as z,n as v}fro
         </dd>
       </dl>
       </section>
-      </template>`}renderEditor(){const{name:e,nickname:t,home:a,airports:r=[],avatar:i,color:s}=this.traveler||{};function n(d,c=""){return h`<input
+      </template>`}renderEditor(){const{name:e,nickname:t,home:r,airports:a=[],avatar:i,color:s}=this.traveler||{};function n(d,c=""){return h`<input
         id="${d}-input"
         name="${d}"
         value=${c}>`}function o(d,c){return h`<label for="${d}-input">${c}</label>`}return h`
       <mu-form @mu-form:submit=${d=>{this.src&&this.handleSubmit(this.src,d.detail)}}>
-        <label>
-          <span>Avatar</span>
-
-        </label>
         <img src=${i} alt=${e} />
         <h1>${n("name",e)}</h1>
         <dl>
@@ -48,17 +44,17 @@ import{i as $,O as f,d as b,f as y,x as h,r as w,h as k,a as _,b as z,n as v}fro
           <dt>${o("nickname","Nickname")}</dt>
           <dd>${n("nickname",t)}</dd>
           <dt>${o("home","Home City")}</dt>
-          <dd>${n("home",a)}</dd>
+          <dd>${n("home",r)}</dd>
           <dt>${o("airports","Airports")}</dt>
           <dd>
-            ${n("airports",r.join(", "))}
+            ${n("airports",a.join(", "))}
           </dd>
           <dt>${o("color","Favorite Color")}</dt>
           <dd>
             <input id="color-input" type="color" name="color" value="${s}">
           </dd>
         </dl>
-      </mu-form>`}connectedCallback(){super.connectedCallback(),this._authObserver.observe(e=>{this._user=e.user,this.src&&this.hydrate(this.src)})}get authorization(){return this._user&&this._user.authenticated?{Authorization:`Bearer ${this._user.token}`}:{}}hydrate(e){fetch(e,{headers:this.authorization}).then(t=>{if(t.status!==200)throw`Status: ${t.status}`;return t.json()}).then(t=>this.traveler=t).catch(t=>console.log(`Failed to render data ${e}:`,t))}handleSubmit(e,t){this._avatar&&(t.avatar=this._avatar),fetch(e,{headers:{"Content-Type":"application/json",...this.authorization},method:"PUT",body:JSON.stringify(t)}).then(a=>{if(a.status!==200)throw`Status: ${a.status}`;return a.json()}).then(a=>{const r=a;this.traveler=r,this.mode="view"})}handleAvatarSelected(e){e&&e.length&&new Promise((a,r)=>{const i=new FileReader;i.onload=()=>a(i.result),i.onerror=s=>r(s),i.readAsDataURL(e[0])}).then(a=>this._avatar=a)}};u.uses=b({"mu-form":y.Element}),u.styles=[w.styles,k.styles,_`
+      </mu-form>`}connectedCallback(){super.connectedCallback(),this._authObserver.observe(e=>{this._user=e.user,this.src&&this.hydrate(this.src)})}get authorization(){return this._user&&this._user.authenticated?{Authorization:`Bearer ${this._user.token}`}:{}}hydrate(e){fetch(e,{headers:this.authorization}).then(t=>{if(t.status!==200)throw`Status: ${t.status}`;return t.json()}).then(t=>this.traveler=t).catch(t=>console.log(`Failed to render data ${e}:`,t))}handleSubmit(e,t){this._avatar&&(t.avatar=this._avatar),fetch(e,{headers:{"Content-Type":"application/json",...this.authorization},method:"PUT",body:JSON.stringify(t)}).then(r=>{if(r.status!==200)throw`Status: ${r.status}`;return r.json()}).then(r=>{const a=r;this.traveler=a,this.mode="view"})}handleAvatarSelected(e){e&&e.length&&new Promise((r,a)=>{const i=new FileReader;i.onload=()=>r(i.result),i.onerror=s=>a(s),i.readAsDataURL(e[0])}).then(r=>this._avatar=r)}};u.uses=y({"mu-form":b.Element}),u.styles=[w.styles,k.styles,_`
     :host {
       display: contents;
       grid-column: 2/-2;
