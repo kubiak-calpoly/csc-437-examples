d2373c53e0f47e06f738b3bcd850745b30d00513
Ken Kubiak <kubiak@calpoly.edu>
2026-04-28 08:48:28 -0700

    remove unused click handler from itinerary

--- packages/proto/public/js/blz-itinerary.js
+++ packages/proto/public/js/blz-itinerary.js
@@ -5,33 +5,6 @@ export class BlzItineraryElement extends HTMLElement {
     super();
     shadow(this)
       .styles(BlzItineraryElement.styles)
-
-    this.addEventListener("click", (event) => {
-      console.log("Click bubbled", event);
-      const button = event.originalTarget;
-      const id = button.id;
-      switch (id) {
-        case "extend-stay":
-          const itinerary = this.viewModel.get("itinerary");
-          const destinations = itinerary.destinations;
-          const rome = destinations[2];
-          let endDate = new Date(rome.endDate);
-          endDate.setTime(
-            endDate.getTime() + 24 * 60 * 60 * 1000
-          );
-          console.log("new end date:", endDate);
-          const newItinerary = {
-            ...itinerary,
-            destinations: destinations.toSpliced(2, 1, {
-              ...rome,
-              endDate: endDate.toISOString().substr(0, 10)
-            })
-          };
-          console.log("Setting itinerary:", newItinerary);
-          this.viewModel.set("itinerary", newItinerary);
-          return;
-      }
-    });
   }
 
   static observedAttributes = ["src"];
