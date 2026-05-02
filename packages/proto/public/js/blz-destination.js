30a034b6206eca2871c513e0ee76e078799dfa2e
Ken Kubiak <kubiak@calpoly.edu>
2026-05-02 09:35:55 -0700

    get viewModel and view to work

--- packages/proto/public/js/blz-destination.js
+++ packages/proto/public/js/blz-destination.js
@@ -12,17 +12,16 @@ export class BlzDestinationElement extends HTMLElement {
   });
 
   view = html`
-    <section>
+    <section
+      style=${($) => `--img-src: url(${$.featuredImage})`}
+    >
       <header>
         <h2>
           <a href=${($) => $.link}>
             <slot>Unnamed Destination</slot>
           </a>
         </h2>
-        <p>${($) => {
-          console.log("$=", $);
-          return nightsBetween($.startDate, $.endDate)
-        }} nights</p>
+        <p>${($) => nightsBetween($.startDate, $.endDate)} nights</p>
       </header>
       <slot name="highlights"></slot>
     </section>
@@ -44,7 +43,6 @@ export class BlzDestinationElement extends HTMLElement {
   ];
 
   attributeChangedCallback(name, _, newValue) {
-    console.log("AttributeChangedCallback", name, newValue);
     switch (name) {
       case "href":
         this.viewModel.set("link", newValue);
@@ -53,7 +51,7 @@ export class BlzDestinationElement extends HTMLElement {
         this.viewModel.set("featuredImage", newValue);
         break;
       case "start-date":
-        this.viewModel.$["startDate"] = newValue;
+        this.viewModel.set("startDate", newValue);
         break;
       case "end-date":
         this.viewModel.set("endDate", newValue);
@@ -64,25 +62,28 @@ export class BlzDestinationElement extends HTMLElement {
   static styles = css`
     :host {
       --img-src: none;
+      --color-text: var(--color-text-inverted);
+      --color-link: var(--color-link-inverted);
     }
     section {
       padding: var(--size-spacing-medium);
       aspect-ratio: 16/9;
       background-image: var(--img-src);
       background-size: cover;
-      color: var(--color-text-inverted);
+      color: var(--color-text);
       text-shadow: var(--shadow-light);
     }
-    h1, h2, h3, h4, h5, h6 {
+    header {
       font-family: var(--font-family-display);
       line-height: var(--font-line-height-display);
+      font-style: italic;
+      text-align: right;
     }
     h2 {
      font-size: var(--size-type-xlarge);
-      font-weight: var(--font-weight-bold);
     }
     a[href] {
-      color: inherit;
+      color: var(--color-link);
     }
   `;
 }
